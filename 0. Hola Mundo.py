0486bebeb7a8ba1dd6305fa21b1fe7ee67e08902
Kevinferrer-19273 <ferrerkevin238@gmail.com>
2026-09-24 11:39:38 -0400

    Add files via upload

---
+++ 0. Hola Mundo.py
@@ -0,0 +1,20 @@
+lista = [1,2,3,4,5]
+
+print(lista)
+
+lista[1]=7
+
+
+lista.append(6)
+print(lista)
+
+lista.insert(2, 2.5)
+print(lista)
+
+lista.remove(2.5)
+print(lista)
+
+lista.pop (3)
+print(lista)
+
+print("numero de elemento",len(lista))
